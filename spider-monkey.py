efffd2a7fd9284cf9c3ded97988013a0fc519d42
brent <hand.brent@gmail.com>
2015-10-19 21:28:16 -0400

    Cleaned up code

--- spider-monkey.py
+++ spider-monkey.py
@@ -18,18 +18,18 @@ while urls != []:
         html = urllib.urlopen(urls[0]).read()
     except:
         print("Error" + url[0])
-    
+
     soup = BeautifulSoup(html)
-    
+
     urls.pop(0)
-    
+
     for tag in soup.findAll('a', href=True):
         tag = urlparse.urljoin(url,tag['href'])
 
         if url in tag and tag not in history:
             urls.append(tag)
             history.append(tag)
-    
+
     time.sleep(5)
 
 print(history)

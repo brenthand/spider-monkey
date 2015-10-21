5edcd30cc47257aa3ec174b4edab68117e1f0511
brent <hand.brent@gmail.com>
2015-10-21 17:47:20 -0400

    added argument handling and indexing

--- spider-monkey.py
+++ spider-monkey.py
@@ -2,13 +2,22 @@ import urlparse
 import urllib
 import time
 from bs4 import BeautifulSoup
+import sys
 
 #
 # Site Mapper
 #
 #
 
-url = "http://brentonwadehand.com"
+if len(sys.argv) < 2:
+    print("Too few arguments")
+    print("Usage: python spider-monkey.py http://example.com")
+    exit(0)
+
+
+index_list = []
+
+url = sys.argv[1] # "http://brentonwadehand.com"
 
 urls = [url]
 history = [url]
@@ -20,7 +29,7 @@ while urls != []:
         print("Error" + url[0])
 
     soup = BeautifulSoup(html)
-
+    index_list.append(urls[0])
     urls.pop(0)
 
     for tag in soup.findAll('a', href=True):
@@ -30,8 +39,8 @@ while urls != []:
             urls.append(tag)
             history.append(tag)
 
-    time.sleep(5)
+    time.sleep(1)
 
-print(history)
+print(index_list)
 
 
